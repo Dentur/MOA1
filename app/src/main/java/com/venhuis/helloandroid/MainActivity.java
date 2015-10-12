f501265a348d7c2be83c37c9725ada98990dbab3
Sebastian Venhuis <sebastian@venhuis.com>
2015-10-12 12:39:19 +0200

    init

---
+++ app/src/main/java/com/venhuis/helloandroid/MainActivity.java
@@ -0,0 +1,30 @@
+package com.venhuis.helloandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //LinearLayout lay = new LinearLayout(this);
+        //lay.setOrientation(LinearLayout.VERTICAL);
+        //TextView text = new TextView(this);
+        //text.setText("Hello Android!");
+        //lay.addView(text);
+        //text = new TextView(this);
+        //text.setText("Hello Android!");
+        //lay.addView(text);
+        //text = new TextView(this);
+        //text.setText("Hello Android!");
+        //lay.addView(text);
+
+        //TextView tv = new TextView(this);
+        //tv.setText("My second Android App");
+        //setContentView(lay);
+    }
+}

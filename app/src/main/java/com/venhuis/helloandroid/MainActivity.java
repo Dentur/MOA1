480115ad729c2d6e911a0ef42507f3ca18e95611
Sebastian Venhuis <sebastian@venhuis.com>
2015-10-26 10:30:40 +0100

    blub

--- app/src/main/java/com/venhuis/helloandroid/MainActivity.java
+++ app/src/main/java/com/venhuis/helloandroid/MainActivity.java
@@ -1,7 +1,9 @@
 package com.venhuis.helloandroid;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -10,21 +12,62 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //LinearLayout lay = new LinearLayout(this);
-        //lay.setOrientation(LinearLayout.VERTICAL);
-        //TextView text = new TextView(this);
-        //text.setText("Hello Android!");
-        //lay.addView(text);
-        //text = new TextView(this);
-        //text.setText("Hello Android!");
-        //lay.addView(text);
-        //text = new TextView(this);
-        //text.setText("Hello Android!");
-        //lay.addView(text);
+        //setContentView(R.layout.activity_main);
+        LinearLayout lay = new LinearLayout(this);
+        lay.setOrientation(LinearLayout.VERTICAL);
+        TextView text = new TextView(this);
+        text.setText("Hello Android!");
+        lay.addView(text);
+        text = new TextView(this);
+        text.setText("Hello Android!");
+        lay.addView(text);
+        text = new TextView(this);
+        text.setText("Hello Android!");
+        lay.addView(text);
 
-        //TextView tv = new TextView(this);
-        //tv.setText("My second Android App");
-        //setContentView(lay);
+        TextView tv = new TextView(this);
+        tv.setText("My second Android App");
+        setContentView(lay);
+        Log.d("seb", "onCreate");
+    }
+
+    @Override
+    protected  void onStart()
+    {
+        super.onStart();
+        Log.d("seb", "onStart");
+    }
+
+    @Override
+    protected  void onRestart()
+    {
+        super.onRestart();
+        Log.d("seb", "onRestart");
+    }
+
+    @Override
+    protected  void onResume()
+    {
+        super.onResume();
+        Log.d("seb", "onResume");
+    }
+
+    @Override
+    protected  void onPause()
+    {
+        super.onPause();
+        Log.d("seb", "onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("seb", "onDestroy");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("seb", "onStop");
     }
 }
